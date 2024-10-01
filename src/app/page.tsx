4bbe70364649db2fed14ac28e72aea71f1bbcd9e
josh mclain <joshmclain45@gmail.com>
2024-10-01 12:22:07 -0700

    :dizzy: (home page): github icon link animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,44 @@
 import clsx from "clsx";
 import { motion } from "framer-motion";
 import { whisper } from "@/global/fonts";
+import IconLink from "@/components/common/IconLink";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen max-w-[1200px] mx-auto">
-      <motion.h1 
-      className={clsx(whisper.className, 'text-5xl md:text-7xl xl:text-8xl mb-2 md:mb-4')}
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      >Mystical Realms</motion.h1>
+      <motion.h1
+        className={clsx(
+          whisper.className,
+          "text-5xl md:text-7xl xl:text-8xl mb-2 md:mb-4"
+        )}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        Mystical Realms
+      </motion.h1>
       <motion.p
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-      >Discover the mystic in the realm of you</motion.p>
+        transition={{ duration: 0.5, delay: 0.2, type: "spring" }}
+      >
+        Discover the mystic in the realm of you
+      </motion.p>
+      <motion.p
+        className="mt-8 text-orange-400"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5, delay: 1, type: "spring" }}
+      >
+        Coming Soon
+      </motion.p>
+      <motion.div
+        className="mt-8"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5, delay: 1.5, type: "spring" }}
+      >
+        <IconLink name="Github" size={64} />
+      </motion.div>
     </div>
   );
 }
