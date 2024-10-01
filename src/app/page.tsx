96431bdea7c1ae74bf90b7fdde8397c336511037
josh mclain <joshmclain45@gmail.com>
2024-10-01 11:05:48 -0700

    :dizzy: (home hero): animated home page text heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import clsx from "clsx";
+"use client";
 
+import clsx from "clsx";
+import { motion } from "framer-motion";
 import { whisper } from "@/global/fonts";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen max-w-[1200px] mx-auto">
-      <h1 className={clsx(whisper.className, 'text-5xl md:text-7xl xl:text-8xl mb-2 md:mb-4')}>Mystical Realms</h1>
-      <p>Discover the mystic in the realm of you</p>
+      <motion.h1 
+      className={clsx(whisper.className, 'text-5xl md:text-7xl xl:text-8xl mb-2 md:mb-4')}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      >Mystical Realms</motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >Discover the mystic in the realm of you</motion.p>
     </div>
   );
 }
