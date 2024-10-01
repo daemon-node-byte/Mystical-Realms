7819ee6f33b0630eaea9f552188dd45d22ed63ff
josh mclain <joshmclain45@gmail.com>
2024-10-01 10:12:24 -0700

    :recycle: (home): remove boilerplate code

---
+++ src/global/fonts.ts
@@ -0,0 +1,13 @@
+import { Whisper, Didact_Gothic } from "next/font/google";
+
+export const whisper = Whisper({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const didactGothic = Didact_Gothic({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
